ee0d1f5ddc49aba5c222abcec345c257c0b83391
Ambyjkl <ambareeshbalaji@gmail.com>
2016-09-30 01:10:23 -0400

    Final commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageFilter
 parser=ArgumentParser(description='Convert images to ASCII art')
 parser.add_argument('--width', '-w', type=int, default=100, help='width of the output in characters')
 #parser.add_argument('--font-size', '-f', type=int, default=12, help='size of the font in px')
-parser.add_argument('--font-ratio', '-r', type=float, default=0.7, help='ratio of the width of the font to the height of the font')
+parser.add_argument('--font-ratio', '-r', type=float, default=0.5, help='ratio of the width of the font to the height of the font')
 parser.add_argument('--output', '-o', help='output file (defaults to filename.txt)')
 parser.add_argument('image', help='input image file')
 args = parser.parse_args()
@@ -36,6 +36,14 @@ print(img.format, img.size, img.mode)
 
 width, height = img.size
 
-#for i in range(0, height-1):
-#	for j in range(0, width-1):
-#		print(ASCIIChar(im[i][j]))
+def asciiChar(intensityTuple):
+    intensity=intensityTuple[0]
+    asciiString=' .:-=+*#%@'
+    newIntensity=9-int(round(intensity*10/255, 0))
+    return asciiString[newIntensity]
+output=""
+for i in range(0, height-1):
+	for j in range(0, width-1):
+		output+=asciiChar(im[j,i])
+	output+='\n'
+print(output)
