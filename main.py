7fdb94d596902c0016e20b13002c51e9a9b1e3d5
Yash Mathur <yash97mathur@gmail.com>
2016-09-29 23:32:34 -0400

    converting size to user's character preference

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from argparse import ArgumentParser
 from PIL import Image, ImageFilter
 
 parser=ArgumentParser(description='Convert images to ASCII art')
-parser.add_argument('--width', '-w', type=int, default=100, help='width of the output in characters')
+parser.add_argument('--width', '-w', type=int, default=200, help='width of the output in characters')
 parser.add_argument('--font-size', '-f', type=int, default=12, help='size of the font in px')
-parser.add_argument('--font-ratio', '-r', type=float, default=1.0, help='ratio of the width of the font to the height of the font')
+parser.add_argument('--font-ratio', '-r', type=float, default=1, help='ratio of the width of the font to the height of the font')
 parser.add_argument('--output', '-o', help='output file (defaults to filename.txt)')
 parser.add_argument('image', help='input image file')
 args = parser.parse_args()
@@ -17,9 +17,11 @@ except:
 
 
 # Resizing the image
-print(type(args))
-print('\n')
-basewidth = (args.font-size/args.font-ratio)*args.width
+#print(type(args))
+#print(dir(args))
+#print(args.font_ratio)
+#print('\n')
+basewidth = int((args.width/args.font_ratio))
 wpercent = (basewidth/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
 img = img.resize((basewidth, hsize))
@@ -27,6 +29,6 @@ img = img.resize((basewidth, hsize))
 
 img.save('grayscale.png')
 im = img.load();
-print(im[1, 1])
-#print("Size of image: ")
-#print(img.format, img.size, img.mode)
+#print(im[1, 1])
+print("Size of image: ")
+print(img.format, img.size, img.mode)
