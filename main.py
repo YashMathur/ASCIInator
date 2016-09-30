b4f695a1b985c72783b46a0ad804c21cfe3015a9
Yash Mathur <yash97mathur@gmail.com>
2016-09-30 00:11:57 -0400

    Implement resize image

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from argparse import ArgumentParser
 from PIL import Image, ImageFilter
 
 parser=ArgumentParser(description='Convert images to ASCII art')
-parser.add_argument('--width', '-w', type=int, default=200, help='width of the output in characters')
-parser.add_argument('--font-size', '-f', type=int, default=12, help='size of the font in px')
-parser.add_argument('--font-ratio', '-r', type=float, default=1, help='ratio of the width of the font to the height of the font')
+parser.add_argument('--width', '-w', type=int, default=100, help='width of the output in characters')
+#parser.add_argument('--font-size', '-f', type=int, default=12, help='size of the font in px')
+parser.add_argument('--font-ratio', '-r', type=float, default=0.7, help='ratio of the width of the font to the height of the font')
 parser.add_argument('--output', '-o', help='output file (defaults to filename.txt)')
 parser.add_argument('image', help='input image file')
 args = parser.parse_args()
@@ -21,9 +21,9 @@ except:
 #print(dir(args))
 #print(args.font_ratio)
 #print('\n')
-basewidth = int((args.width/args.font_ratio))
-wpercent = (basewidth/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
+basewidth = args.width
+wpercent = float((basewidth/float(img.size[0])))
+hsize = int((float(img.size[1])*wpercent*args.font_ratio))
 img = img.resize((basewidth, hsize))
 
 
